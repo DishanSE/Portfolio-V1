0047be4fe77db4f3f0a5846c33265af4e4dd9342
DishanSE <dishanraj13@gmail.com>
2025-07-11 13:58:43 +0530

    tech stack added.

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -104,18 +104,23 @@ function a11yProps(index) {
 
 // techStacks tetap sama
 const techStacks = [
+  { icon: "python.svg", language: "Python" },
+  { icon: "java.svg", language: "Java" },
   { icon: "html.svg", language: "HTML" },
   { icon: "css.svg", language: "CSS" },
   { icon: "javascript.svg", language: "JavaScript" },
-  { icon: "tailwind.svg", language: "Tailwind CSS" },
+  { icon: "php.svg", language: "PHP" },
+  { icon: "mysql.svg", language: "MySQL" },
   { icon: "reactjs.svg", language: "ReactJS" },
   { icon: "vite.svg", language: "Vite" },
   { icon: "nodejs.svg", language: "Node JS" },
   { icon: "bootstrap.svg", language: "Bootstrap" },
   { icon: "firebase.svg", language: "Firebase" },
-  { icon: "MUI.svg", language: "Material UI" },
-  { icon: "vercel.svg", language: "Vercel" },
-  { icon: "SweetAlert.svg", language: "SweetAlert2" },
+  { icon: "xampp.svg", language: "XAMPP" },
+  { icon: "figma.svg", language: "Figma" },
+  { icon: "netlify.svg", language: "Netlify" },
+  { icon: "git.svg", language: "Git" },
+  { icon: "github.svg", language: "GitHub" },
 ];
 
 export default function FullWidthTabs() {
